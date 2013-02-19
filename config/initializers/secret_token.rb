1504ffc9c8a629b906e2fbf92f72c7ead568fe92
unknown <Besitzer@Notebook.(none)>
2013-02-19 17:32:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoBook::Application.config.secret_token = '6bb753126c380539f57b7114f70e284cef7b7c1255c576c159a960591341b38c756cada4bf39a6291b9ea61d5add0d1721c4eb6e481ed73bee507541e4e63525'
