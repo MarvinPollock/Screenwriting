a9eec706d5edeb3a54aa039c8093d7b9b4592dcb
unknown <Besitzer@Notebook.(none)>
2013-02-21 16:50:33 +0100

    Zwischenspeicher

--- features/users/visit_pages_steps.rb
+++ features/users/visit_pages_steps.rb
@@ -1,5 +1,7 @@
 Given /^I am at page users$/ do
-
+  all('h1').each do |h|
+    h.to_s.match("CXS")
+  end
 end
 
 When /^I click at Projects$/ do
@@ -7,5 +9,5 @@ When /^I click at Projects$/ do
 end
 
 Then /^I should be redirected to Projects$/ do
-  #pending # express the regexp above with the code you wish you had
+  ''.nil?
 end
