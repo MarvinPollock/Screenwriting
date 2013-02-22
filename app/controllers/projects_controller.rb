f0212f6766affa800a05d9da48a01fb4ff3bbab4
unknown <Besitzer@Notebook.(none)>
2013-02-22 17:24:15 +0100

    Erste Umsetung der GUI, horizontal scrollbar, Pads werden erkannt.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,7 +15,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1.json
   def show
     @project = Project.find(params[:id])
-
+    @pads = Pad.find_all_by_story(@project.name)
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @project }
